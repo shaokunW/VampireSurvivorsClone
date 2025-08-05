e088c04682647815befd527c7819f8ddf9050950
falling-W <wangsk15@outlook.com>
2025-08-05 14:02:23 +0800

    Initialize Reward/Loot/Items

--- Assets/Scripts/Core/EnemyController.cs
+++ Assets/Scripts/Core/EnemyController.cs
@@ -1,25 +1,42 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 namespace Vampire
 {
     public class EnemyController : MonoBehaviour
     {
-        private float maxHp;
-        private float currentHp;
-        private float currentSpeed;
-        public  event Action<EnemyController> OnDeactivated;
-        
+        public StatsController statsController;
+        public event Action<EnemyController> OnDeactivated;
+        public EnemyData enemyData;
+        public bool killedEventSent;
+        public GameEvent<EnemyDiedEventData> DiedEvent;
+
+        public void Awake()
+        {
+            statsController = GetComponent<StatsController>();
+        }
+
         public void Initialize(EnemyData data)
         {
-            
+            this.enemyData = data;
+            this.killedEventSent = false;
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (statsController.Die())
+            {
+                if (!killedEventSent)
+                {
+                    var diedEvent = new EnemyDiedEventData();
+                    diedEvent.EnemyID = enemyData.id;
+                    diedEvent.EnemyPosition = transform.position;
+                    DiedEvent.Raise(diedEvent);
+                    killedEventSent = true;
+                    OnDeactivated?.Invoke(this);
+                }
+            }
         }
     }
 }
--- Assets/Scripts/Monsters/EnemyData.cs
+++ Assets/Scripts/Monsters/EnemyData.cs
@@ -11,7 +11,6 @@ namespace Vampire
         [Header("基础信息")] [Tooltip("敌人的唯一ID，用于索引和查找。")]
         public string id;
 
-        [Tooltip("敌人的外观预制体。使用AssetReference以兼容Addressables系统。")]
         public AssetReferenceSprite graphicsId;
 
         [Header("生命值 (HP)")] [Tooltip("怪物的基础生命值。")]
@@ -28,15 +27,7 @@ namespace Vampire
 
         [Tooltip("每一波到来时，伤害额外增加的量。最终伤害 = Floor(基础伤害 + 波数 * 每波伤害提升)")]
         public float damagePerWave;
-
-        [Header("掉落物 (Drops)")] [Tooltip("击败时掉落的固定资源数量。")]
-        public int resourceDropAmount;
-
-        [Tooltip("掉落回血道具的概率，范围 0-100。")] [Range(0, 100)]
-        public int healthDropChance;
-
-        [Tooltip("掉落宝箱的概率，范围 0-100。")] [Range(0, 100)]
-        public int chestDropChance;
+        
 
         [Header("事件")] [Tooltip("击败后触发的事件函数名列表（用于反射调用）。")]
         public List<string> onDefeatEvents;
